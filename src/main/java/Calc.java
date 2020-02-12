b15b66741c22e9dc85c6d85e657d59c02beb0e05
rubtsovnyu <-e9CmG4ZTf=A_YHS>
2020-02-12 21:17:39 +0300

    use BigDecimal instead of double primitives, GUI done, add about info, so it works

---
+++ src/main/java/Calc.java
@@ -0,0 +1,35 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+public class Calc {
+    private BigDecimal currentResult;
+
+    public Calc(final BigDecimal initialValue) {
+        this.currentResult = initialValue;
+    }
+
+    public Calc() {
+        this(new BigDecimal(BigInteger.ZERO).setScale(15, RoundingMode.HALF_UP));
+    }
+
+    BigDecimal add(final BigDecimal val) {
+        currentResult = currentResult.add(val);
+        return currentResult;
+    }
+
+    BigDecimal sub(final BigDecimal val) {
+        currentResult = currentResult.subtract(val);
+        return currentResult;
+    }
+
+    BigDecimal divide(final BigDecimal val) {
+        currentResult = currentResult.divide(val, RoundingMode.HALF_UP);
+        return currentResult;
+    }
+
+    BigDecimal multiply(final BigDecimal val) {
+        currentResult = currentResult.multiply(val);
+        return currentResult;
+    }
+}
--- src/main/java/CalcWindow.java
+++ src/main/java/CalcWindow.java
@@ -1,23 +1,59 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class CalcWindow extends JFrame {
-    private static final Dimension windowDimension = new Dimension(300, 300);
-    private static final Dimension fieldsDimension = new Dimension(300, 50);
-
+    public static final String DIVIDE = "/";
+    public static final String MULTIPLY = "*";
+    public static final String SUBTRACT = "-";
+    public static final String ADD = "+";
+    public static final String DOT = ".";
+    public static final String CALCULATE = "=";
+    public static final String ZERO = "0";
+    public static final String EMPTY_STRING = "";
+    public static final String CLEAR = "C";
+    public static final String ABOUT_ICON_PATH = "/icon/about_icon.png";
+    private static final Dimension windowDimension = new Dimension(400, 300);
+    private static final Dimension fieldsDimension = new Dimension(400, 50);
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private BoxLayout mainLayout;
+    private Calc calc;
+    private JTextField currentNumberField;
+    private JLabel clickCount;
+    private String lastOperation = EMPTY_STRING;
+    private JTextField tempResultField;
+    final ActionListener calcBtnAListener = e -> {
+        final JButton button = (JButton) e.getSource();
+        updateClickCount();
+        handleButtonClick(button.getText());
+    };
 
     public CalcWindow() throws HeadlessException {
         super("Calculator");
         setMinimumSize(windowDimension);
+        calc = new Calc();
         initGUI();
         pack();
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new CalcWindow());
+        SwingUtilities.invokeLater(CalcWindow::new);
+    }
+
+    static void showErrorDialog(final String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    static void showInfoDialog(final String message) {
+        JOptionPane.showMessageDialog(null, message, "About", JOptionPane.INFORMATION_MESSAGE);
     }
 
     private void initGUI() {
@@ -27,12 +63,13 @@ public class CalcWindow extends JFrame {
         contentPane.setLayout(mainLayout);
 
         final JLabel tempResultLabel = new JLabel("Intermediate result:");
-        final JTextField tempResultField = new JTextField();
+        tempResultField = new JTextField();
         tempResultField.setHorizontalAlignment(SwingConstants.RIGHT);
         tempResultField.setEditable(false);
 
         final JPanel tempResPanel = new JPanel();
-        tempResPanel.setMaximumSize(fieldsDimension);
+//        tempResPanel.setMaximumSize(fieldsDimension);
+        tempResPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         final BoxLayout tempResBoxLayout = new BoxLayout(tempResPanel, BoxLayout.Y_AXIS);
         tempResPanel.setLayout(tempResBoxLayout);
         tempResPanel.add(tempResultLabel);
@@ -40,12 +77,13 @@ public class CalcWindow extends JFrame {
         contentPane.add(tempResPanel);
 
         final JLabel currentNumberLabel = new JLabel("Enter a number:");
-        final JTextField currentNumberField = new JTextField();
+        currentNumberField = new JTextField();
         currentNumberField.setHorizontalAlignment(SwingConstants.RIGHT);
         currentNumberField.setEditable(false);
 
         final JPanel currentNumberPanel = new JPanel();
-        currentNumberPanel.setMaximumSize(fieldsDimension);
+//        currentNumberPanel.setMaximumSize(fieldsDimension);
+        currentNumberPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         final BoxLayout currentNumberBoxLayout = new BoxLayout(currentNumberPanel, BoxLayout.Y_AXIS);
         currentNumberPanel.setLayout(currentNumberBoxLayout);
         currentNumberPanel.add(currentNumberLabel);
@@ -56,14 +94,21 @@ public class CalcWindow extends JFrame {
         final JPanel digitPanel = new JPanel();
         digitPanel.setLayout(digitLayout);
 
-        final ArrayList<JButton> buttonList = new ArrayList<>(10);
-        for (int i = 0; i < 10; i++) {
-            buttonList.add(new JButton(String.valueOf(i)));
-            digitPanel.add(buttonList.get(i));
+        for (int i = 2; i > -1; i--) {
+            for (int j = i * 3 + 1; j < i * 3 + 4; j++) {
+                final JButton digitButton = new JButton(String.valueOf(j));
+                digitButton.addActionListener(calcBtnAListener);
+                digitPanel.add(digitButton);
+            }
         }
 
-        final JButton btnComma = new JButton(",");
-        final JButton btnCalc = new JButton("=");
+        final JButton btnZero = new JButton(ZERO);
+        btnZero.addActionListener(calcBtnAListener);
+        final JButton btnComma = new JButton(DOT);
+        btnComma.addActionListener(calcBtnAListener);
+        final JButton btnCalc = new JButton(CALCULATE);
+        btnCalc.addActionListener(calcBtnAListener);
+        digitPanel.add(btnZero);
         digitPanel.add(btnComma);
         digitPanel.add(btnCalc);
 
@@ -71,35 +116,45 @@ public class CalcWindow extends JFrame {
         final JPanel opersPanel = new JPanel();
         opersPanel.setLayout(opersLayout);
 
-        final JButton btnDiv = new JButton("/");
-        final JButton btnMultiply = new JButton("*");
-        final JButton btnSub = new JButton("-");
-        final JButton btnAdd = new JButton("+");
+        final ArrayList<JButton> operBtnList = new ArrayList<>(4);
+        operBtnList.add(new JButton(DIVIDE));
+        operBtnList.add(new JButton(MULTIPLY));
+        operBtnList.add(new JButton(SUBTRACT));
+        operBtnList.add(new JButton(ADD));
 
-        opersPanel.add(btnDiv);
-        opersPanel.add(btnMultiply);
-        opersPanel.add(btnSub);
-        opersPanel.add(btnAdd);
+        operBtnList.forEach(b -> {
+            b.addActionListener(calcBtnAListener);
+            opersPanel.add(b);
+        });
 
         final JPanel buttonPanel = new JPanel();
         final GridBagLayout buttonsLayout = new GridBagLayout();
         buttonPanel.setLayout(buttonsLayout);
-        GridBagConstraints constraints = new GridBagConstraints();
+        final GridBagConstraints constraints = new GridBagConstraints();
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 0.75;
+        constraints.gridx = 0;
         constraints.gridy = 0;
-        constraints.gridx = 0;      // нулевая ячейка таблицы по горизонтали
         buttonPanel.add(digitPanel, constraints);
 
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 0.25;
-        constraints.gridx = 1;      // нулевая ячейка таблицы по горизонтали
+        constraints.gridx = 1;
         buttonPanel.add(opersPanel, constraints);
+
+        final JButton clearButton = new JButton(CLEAR);
+        clearButton.addActionListener(calcBtnAListener);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0;
+        constraints.gridwidth = 4;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        buttonPanel.add(clearButton, constraints);
 
         contentPane.add(buttonPanel);
 
         final JLabel clickCountDesc = new JLabel("Click count: ");
-        final JLabel clickCount = new JLabel("0");
+        clickCount = new JLabel(ZERO);
 
         final JPanel clickPanel = new JPanel();
         final BoxLayout clickLayout = new BoxLayout(clickPanel, BoxLayout.X_AXIS);
@@ -108,5 +163,151 @@ public class CalcWindow extends JFrame {
         clickPanel.add(clickCount);
 
         contentPane.add(clickPanel);
+
+        ImageIcon aboutIcon = null;
+        try {
+            aboutIcon = new ImageIcon(ImageIO.read(getClass().getResourceAsStream(ABOUT_ICON_PATH)));
+            aboutIcon.setImage(aboutIcon.getImage().getScaledInstance(24, 24, Image.SCALE_SMOOTH));
+        } catch (IOException e) {
+            logger.error("Can't load about icon", e);
+        }
+
+        final JMenuItem aboutMenuItem = new JMenuItem("About...");
+        if (aboutIcon != null) {
+            aboutMenuItem.setIcon(aboutIcon);
+        }
+
+        aboutMenuItem.addActionListener(e -> showInfoDialog("Author:\nNikolai Rubtsov,\n2020"));
+
+        final JMenu helpMenu = new JMenu("Help");
+        helpMenu.add(aboutMenuItem);
+
+        final JMenuBar menuBar = new JMenuBar();
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+    }
+
+    private void handleButtonClick(final String command) {
+        if (command == null || command.length() != 1) {
+            logger.error("Wrong command (length must be == 1)");
+            throw new IllegalArgumentException("Wrong command!");
+        }
+        try {
+            Integer.parseInt(command);
+            handleDigitButton(command);
+        } catch (NumberFormatException e) {
+            handleOperationButton(command);
+        }
+    }
+
+    private void handleDigitButton(final String digit) {
+        if (lastOperation.equals(CALCULATE)) {
+            clearAll();
+        }
+        currentNumberField.setText(currentNumberField.getText().concat(String.valueOf(digit)));
+    }
+
+    private void handleOperationButton(final String operation) {
+        if (operation.equals(CALCULATE)) {
+            if (!isFieldEmpty(currentNumberField) && (
+                    lastOperation.equals(ADD)
+                            || lastOperation.equals(SUBTRACT)
+                            || lastOperation.equals(MULTIPLY)
+                            || lastOperation.equals(DIVIDE))) {
+                performOperation(lastOperation);
+                lastOperation = CALCULATE;
+            } else {
+                logger.error("You are trying to perform illegal operation! NO.");
+                showErrorDialog("What do you want???");
+            }
+            return;
+        }
+
+        if (operation.equals(DOT)) {
+            if (!currentNumberField.getText().contains(DOT)) {
+                if (lastOperation.equals(CALCULATE)) {
+                    clearAll();
+                }
+                currentNumberField.setText(currentNumberField.getText().concat(DOT));
+            }
+            return;
+        }
+
+        if (operation.equals(CLEAR)) {
+            if (isFieldEmpty(currentNumberField)) {
+                clearAll();
+            } else {
+                clearCurrentNumber();
+            }
+            return;
+        }
+
+        if (operation.equals(SUBTRACT) && isFieldEmpty(currentNumberField) && isFieldEmpty(tempResultField)) {
+            currentNumberField.setText(currentNumberField.getText().concat(SUBTRACT));
+            return;
+        }
+
+        lastOperation = operation;
+
+        if (!isFieldEmpty(currentNumberField)
+                && isFieldEmpty(tempResultField)
+                && !currentNumberField.equals(SUBTRACT)) {
+            tempResultField.setText(currentNumberField.getText());
+            currentNumberField.setText(EMPTY_STRING);
+            calc = new Calc(new BigDecimal(tempResultField.getText()));
+        }
+    }
+
+    private boolean isFieldEmpty(JTextField currentNumberField) {
+        return currentNumberField.getText().isEmpty();
+    }
+
+    private void performOperation(final String operation) {
+        final BigDecimal resultValue;
+        switch (operation) {
+            case DIVIDE:
+                if (new BigDecimal(currentNumberField.getText()).compareTo(BigDecimal.ZERO) == 0) {
+                    logger.error("Division by zero");
+                    showErrorDialog("Division by zero???");
+                    return;
+                }
+                resultValue = calc.divide(new BigDecimal(currentNumberField.getText()));
+                break;
+            case MULTIPLY:
+                resultValue = calc.multiply(new BigDecimal(currentNumberField.getText()));
+                break;
+            case SUBTRACT:
+                resultValue = calc.sub(new BigDecimal(currentNumberField.getText()));
+                break;
+            case ADD:
+                resultValue = calc.add(new BigDecimal(currentNumberField.getText()));
+                break;
+            default:
+                logger.error("You are trying to perform illegal operation! NO.");
+                return;
+        }
+        updateResult(resultValue);
+    }
+
+    private void updateClickCount() {
+        clickCount.setText(String.valueOf(Integer.parseInt(clickCount.getText()) + 1));
+    }
+
+    private void updateResult(final BigDecimal val) {
+        final String value = val.stripTrailingZeros().toPlainString();
+        tempResultField.setText(value);
+        currentNumberField.setText(EMPTY_STRING);
+    }
+
+    private void clearCurrentNumber() {
+        currentNumberField.setText(EMPTY_STRING);
+    }
+
+    private void clearAll() {
+        lastOperation = EMPTY_STRING;
+        tempResultField.setText(EMPTY_STRING);
+        clearCurrentNumber();
+        calc = new Calc();
     }
 }
